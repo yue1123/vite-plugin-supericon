bd2b61febc56ffcaa1395fde1c0fff50381f3993
yue1123 <2572468699@qq.com>
2025-01-02 16:12:16 +0800

    fix(client): support base url

--- src/client/logic/getHot.ts
+++ src/client/logic/getHot.ts
@@ -1,7 +1,9 @@
 import { createHotContext } from 'vite-hot-client'
 
-const hotContext = createHotContext()
-
+const hotContext = createHotContext(
+  '/___',
+  `${location.pathname.split('/__supericon')[0] || ''}/`.replace(/\/\//g, '/')
+)
 export async function getHot() {
   return await hotContext
 }
